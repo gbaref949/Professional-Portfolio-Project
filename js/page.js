8bb29e1cc056cb496ae92747a845b8b1cd656cd7
gbaref949@west-mec.org <gbaref949@west-mec.org>
2022-12-20 14:16:45 -0700

    Major Changes

--- js/page.js
+++ js/page.js
@@ -1,10 +1,14 @@
 /*
-  The color effects and animation are done via CSS. This bit of JavaScript just lets you toggle the luminaires and turns a few on to start.
+  The color effects and animation are done via CSS. This bit of JavaScript just lets you toggle the logo1 and turns a few on to start.
 */
 
-$(function() {
-  $('.luminaire:nth-child(2n)').addClass('on');
-  $('.luminaire').on('click', function() {
-    $(this).toggleClass('on');
-  });
-});
+// const newLocal = newFunction();
+
+// function newFunction() {
+//   return $(function () {
+//     $('.logo1:nth-child(2n)').addClass('on');
+//     $('.logo1').on('click', function () {
+//       $(this).toggleClass('on');
+//     });
+//   });
+// }
